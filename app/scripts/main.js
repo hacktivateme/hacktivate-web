92338f8fe094aad5f77a50e7f7e3aa74625d5092
Sam Hogg <samhogg@me.com>
2013-09-29 13:45:06 +1300

    MOAR CHANGES.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,28 @@
 $(function() {
-	$('.box').css("min-height", $(window).height());
-
+	$('.box, .rslides > li >  div').css("height", $(window).height());
 	$( window ).resize(function() {
-	$('.box').css("min-height", $(window).height());
-	s.refresh();
-	
+		$('.box, .rslides > li > div').css("height", $(window).height());
+		s.refresh();
+	});
+
+	$(".rslides").responsiveSlides({
+	  auto: true,             // Boolean: Animate automatically, true or false
+	  speed: 500,            // Integer: Speed of the transition, in milliseconds
+	  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+	  pager: false,           // Boolean: Show pager, true or false
+	  nav: true,             // Boolean: Show navigation, true or false
+	  prevText: "<",   // String: Text for the "previous" button
+	  nextText: ">",       // String: Text for the "next" button
+	  navContainer: ".rslides",       // Selector: Where controls should be appended to, default is after the 'ul'
+	  manualControls: "",     // Selector: Declare custom pager navigation
 	});
 
 
 	var s = skrollr.init();
 	s.refresh();
+
+
+
+
 
 });
