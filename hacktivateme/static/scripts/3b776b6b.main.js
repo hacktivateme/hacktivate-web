56253cd4c24b2944fbe9cbde2d75bf53521fce52
Sam Hogg <samhogg@me.com>
2013-09-29 13:45:06 +1300

    MOAR CHANGES.

--- hacktivateme/static/scripts/3b776b6b.main.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".box").css("min-height",$(window).height()),$(window).resize(function(){$(".box").css("min-height",$(window).height()),a.refresh()});var a=skrollr.init();a.refresh()});
